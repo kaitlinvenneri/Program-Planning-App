1dcc1e6497c8418ac8eab15d28db5c8ca1b5b69d
Kaitlin <vennerik@uoguelph.ca>
2020-03-09 04:21:56 -0400

    Unit tests for Course.java

---
+++ src/test/java/programplanningapp/CourseTest.java
@@ -0,0 +1,28 @@
+package programplanningapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseTest {
+    private Course courseOne;
+    private String courseOneCourseCode;
+
+    /**
+     *
+     */
+    @Before
+    public void setUp() {
+        courseOneCourseCode = "CIS*4450";
+        courseOne = new Course(courseOneCourseCode);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testGetCode() {
+        courseOne.getCode();
+    }
+}
