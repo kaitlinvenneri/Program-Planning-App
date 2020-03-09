2625329375ca6de34cf3bb871273a3e0f7b8188c
Kaitlin <vennerik@uoguelph.ca>
2020-03-09 03:32:19 -0400

    Testing if unit testing works with CI - test should fail

---
+++ src/test/java/programplanningapp/DataUtilityTest.java
@@ -0,0 +1,60 @@
+package programplanningapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DataUtilityTest {
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void getSavedCoursesFilename() {
+        assertTrue(1 == 2);
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void getStoredAdminCourses() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void updateStoredAdminCourses() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void writeCoursesToFile() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void getSavedProgramsDirectory() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void getStoredPrograms() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void updateStoredPrograms() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void writeProgramsToFile() {
+    }
+
+    /** @noinspection checkstyle:MissingJavadocMethod*/
+    @Test
+    public void getProgramFilename() {
+    }
+}
